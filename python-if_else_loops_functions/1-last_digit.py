caf9457d254d3c5c332229aebf128f9c849b297d
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-09-26 11:11:56 +0200

    Corrected file: 1-last_digit.py added - sign to last digits when it is last digit of a negative number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 number_str = str(number)
-last_digit = number_str[-1]
-print("Last digit of {} is {}".format(number, last_digit), end=" ")
 
+if number < 0:
+    last_digit = "-" + number_str[-1]
+else:
+    last_digit = number_str[-1]
+
+print("Last digit of {} is {}".format(number, last_digit), end=" ")
 if int(last_digit) > 5:
     print("and is greater than 5")
 elif int(last_digit) == 0:
