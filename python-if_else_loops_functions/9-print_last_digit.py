da2286772e8d2622042c2d64227322500f2cc46c
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-09-26 23:53:00 +0200

    Corrected file 9-print_last_digit.py print type

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 
 def print_last_digit(number):
     new_number = str(number)
-    print("{}".format(new_number[-1]), end="")
+    print("{}".format(int(new_number[-1])), end="")
     return int(new_number[-1])
