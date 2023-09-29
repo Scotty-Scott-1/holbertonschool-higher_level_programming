0d6a15e6bf04d271a7d61fa5ea6c487b88a76ec9
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-09-29 13:12:59 +0200

    Corrected task 7

--- python-import_modules/101-easy_print.py
+++ python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-from test import is_cool
-is_cool()
+import test
--- python-import_modules/test.py
+++ python-import_modules/test.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def is_cool():
-    if __name__ == "__main__":
-        print("#pythoniscool")
-is_cool()
+print("#pythoniscool")
+
