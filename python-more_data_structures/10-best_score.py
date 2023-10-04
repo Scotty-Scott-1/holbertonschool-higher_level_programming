9f26d46a060fc630b8485cd9980c697e5162939c
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-04 20:45:54 +0200

    File 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
 
-    if a_dictionary is None or a_dictionary == []:
+    if not a_dictionary or a_dictionary == []:
         return None
 
     result = max(a_dictionary, key=a_dictionary.get)
