e1b72a0d486c312b9d45c7bb08cea63cdc892b19
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-04 23:47:15 +0200

    correcte file 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,9 @@
 def roman_to_int(roman_string):
 
     if roman_string is None:
-        return None
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
 
     my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 100}
     index = 0
