6eb1f2ac8a7425491e902043ec77ed134ba7ac95
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-04 23:33:00 +0200

    Added task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    if roman_string is None:
+        return None
+
+    my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 100}
+    index = 0
+    result = 0
+    current = 0
+    next = 0
+
+    if len(roman_string) == 1:
+        result = my_dict[roman_string[0]]
+        return result
+
+    for index in range(0, len(roman_string) - 1):
+
+        current = my_dict[roman_string[index]]
+        next = my_dict[roman_string[index + 1]]
+
+        if current < next:
+            result -= current
+        else:
+            result += current
+
+    result += my_dict[roman_string[-1]]
+
+    return result
