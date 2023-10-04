8235d30e1bf94b360f570fabc3b6886b55006878
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-04 12:27:48 +0200

    Added task 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+
+    if a_dictionary is None:
+        return
+
+    best_name = ""
+    highest_score = 0
+
+    for name, score in a_dictionary.items():
+        if score > highest_score:
+            highest_score = score
+            best_name = name
+
+    return best_name
