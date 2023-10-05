696ab819f417ba99051d0fe60471a6aa428813fc
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-06 00:04:13 +0200

    task 2 corrected pycode

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,19 +5,12 @@ def safe_print_list_integers(my_list=[], x=0):
         j = 0
 
         for i in range(0, x):
-            print("{:d}".format(my_list[i]), end="")
-            if j <= x:
-                j += 1
-
-    except (ValueError, TypeError):
-        for i in range(i, x):
-            if not isinstance(my_list[i], int):
-                i+=1
-            else:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 if j <= x:
                     j += 1
 
+    except (ValueError, TypeError):
         print()
         return j
 
