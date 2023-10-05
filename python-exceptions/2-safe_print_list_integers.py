fecaabd61c2d69254b594a452894fa5ef9cd136f
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-05 23:08:29 +0200

    Added task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+
+    try:
+        j = 0
+        for i in range(0, x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                if j <= x:
+                    j += 1
+
+    except (TypeError, IndexError, ValueError):
+        print()
+        return j
+
+    print()
+    return j
