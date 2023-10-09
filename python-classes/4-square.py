b37396e56e68814a450b7a5609acfd83218c13c3
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-09 16:35:31 +0200

    Added task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" A class with one privite instance attribute and a public method"""
+
+
+class Square:
+    """A class: sqaure that contains the a field for size
+
+    Attributes:
+        size: privite attr which describles ojbect's size
+    """
+    def __init__(self, size=0):
+        """instation of square object
+
+        Args:
+        size (int): size of the square. Called and set by setter and getter
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """A getter method for calling the value of object.__size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A setter method for setting value of object.__size
+
+        Args:
+        value: the value to be checked for errors then set as object__size
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ A method that returns the area of a sqaure object"""
+        return self.__size * self.__size
