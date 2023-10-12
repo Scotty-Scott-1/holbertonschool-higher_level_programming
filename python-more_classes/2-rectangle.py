25a5abc5fbc3333b1774dfbea929a4bfdfe9300f
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-12 22:53:55 +0200

    Added task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""A class that defines a rectangles"""
+
+
+class Rectangle:
+    """A Rectange class
+    Attributes:
+        height: private
+        width: private
+    """
+    def __init__(self, width=0, height=0):
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """A method that returns the area of object"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """A method that returns the perimeter of object"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return self.__height + self.__height + self.__width + self.__width
