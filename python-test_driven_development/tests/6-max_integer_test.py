d01107a2abfb5a1cd6f372da559f8fc4077c6d32
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-12 11:23:49 +0200

    task 5

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,40 +9,18 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_normal(self):
-        max_integer([1, 2, 3, 4])
-        result = 4
-        self.assertEqual(result, 4)
+    def test_max_iteger(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4, 50]), 50)
+        self.assertEqual(max_integer([100, 2, 3, 4, 50]), 100)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([-1, -2, -3, -4, -50]), -1)
+        self.assertEqual(max_integer("abczde"), "z")
+        self.assertEqual(max_integer(["e"]), "e")
+        self.assertEqual(max_integer([1.00, 1.01, 1.02, 1.03]), 1.03)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 1]), 1)
 
-    def test_normal2(self):
-        max_integer([1, 2, 3, 4, 50])
-        result = 50
-        self.assertEqual(result, 50)
-
-    def test_empty_list(self):
-        max_integer([])
-        result = None
-        self.assertEqual(result, None)
-
-    def test_negative(self):
-        max_integer([-1, -2, -3, -4, -50])
-        result = -1
-        self.assertEqual(result, -1)
-
-    def test_string(self):
-        max_integer(["abczde"])
-        result = "z"
-        self.assertEqual(result, "z")
-
-    def test_char(self):
-        max_integer(["e"])
-        result = "e"
-        self.assertEqual(result, "e")
-
-    def test_float(self):
-        max_integer([1.00, 1.01, 1.02, 1.03])
-        result = 1.03
-        self.assertEqual(result, 1.03)
 
     if __name__ == "__main__":
         unittest.main()
