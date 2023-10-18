41d47e9e26d51f6bcc3c3ae2aaac5d9ebfd5d0a6
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-18 15:55:00 +0200

    Added task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module that defines a class"""
+
+
+class MyList(list):
+    """
+    Mylist: A class that inherits the class list
+
+    Methods:
+        print_sorted: sorts self
+    """
+    def print_sorted(self):
+        """A method that sorts and prints self"""
+        new_list = sorted(self)
+        print(new_list)
