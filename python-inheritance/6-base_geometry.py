377e6a5ee0627d700fb62a160b0185d4694eea92
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-18 20:18:54 +0200

    Added task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a class"""
+
+
+class BaseGeometry:
+    """
+    Class: BaseGeometry
+
+    Methods:
+        area: raises an error
+    """
+
+    def area(self):
+        """raises an error"""
+        raise AttributeError("area() is not implemented")
