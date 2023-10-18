3686c3867030d0026deffdcd25fbd9ae5a76c1db
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-18 18:37:50 +0200

    Comments x2

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,7 +4,8 @@
 
 class MyList(list):
     """Mylist: A class that inherits the class list
-        Methods: print_sorted(self): sorts self"""
+        Methods: print_sorted(self): sorts self
+    """
 
     def print_sorted(self):
         """print list in order"""
