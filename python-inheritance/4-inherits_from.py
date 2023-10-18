5e483ab54f10c18592769ff9bdad423d1cc32627
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-18 19:59:26 +0200

    added task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""checks if obj is of a class that inherits"""
+
+
+def inherits_from(obj, a_class):
+    """checks is obj inherits"""
+    if issubclass(type(obj), a_class):
+        if type(obj) is not a_class:
+            return True
+    else:
+        return False
