9e427875279832ff4ff90c47e434c772484b9f7e
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-20 12:30:46 +0200

    Corrected task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """a module that returns a json dict of an obj"""
-import json
 
 
 def class_to_json(obj):
     """A function that returns a json dict of an obj"""
-    json_dict = json.dumps(obj.__dict__)
+    json_dict = {}
+
+    if hasattr(obj, "__dict__"):
+        json_dict = obj.__dict__.copy()
     return json_dict
