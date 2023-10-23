842fe459e8ec26332a12fca21b4ececc1029e774
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-23 13:32:20 +0200

    Added task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,6 +12,18 @@ class Rectangle(Base):
          __init__(self, width, height, x=0, y=0, id=None):
             initalize an instance of Rectangle
 
+        def width(self, value):
+            setter for width
+
+        def height(self, value):
+            setter fir height
+
+        def x(self, value):
+            setter for x
+
+         def x(self, value):
+            setter for y
+
     Attributes:
          super().__init__(id)
         self.__width: width of rectangle
@@ -23,6 +35,27 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """initialize a rectanlge innstance"""
         super().__init__(id)
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be > 0")
+
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be > 0")
+
         self.__width = width
         self.__height = height
         self.__x = x
@@ -46,16 +79,40 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        """error checks and updates width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = value
 
     @height.setter
     def height(self, value):
+        """error checks and updates height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
         self.__height = value
 
     @x.setter
     def x(self, value):
+        """error checks and updates x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
+
         self.__x = value
 
     @y.setter
     def y(self, value):
+        """error checks and updates y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
+
         self.__y = value
