1a79f86f1af6ada1dde29f16dbdd2614e271a467
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-23 17:21:18 +0200

    Added task 9

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""define a class Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class: Square
+        Parent: Rectangle
+
+    Methods:
+        __init__(self, size, x=0, y=0, id=None):
+            initalize an instance of Square
+
+        __str__(self):
+        [Square] (<id>) <x>/<y> - <size>
+
+    Attributes:
+        self.id: inherited from parent
+        self.x: inherited from parent
+        self.y: inherited from parent
+        self.width: inherited from parent
+        self.height: inherited from parent
+
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize an intance of Square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """[Square] (<id>) <x>/<y> - <size>"""
+        return (
+            "[Square] ({}) {}/{} - {}"
+            .format(self.id, self.x, self.y, self.height)
+            )
