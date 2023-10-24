b27c121f02cffac071b51aa923a6f8a6a2351504
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-24 14:22:03 +0200

    Added task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,6 +15,10 @@ class Base:
         def to_json_string(list_dictionaries):
             converts an object to json string
 
+        @classmethod
+        def save_to_file(cls, list_objs):
+            write a json string representation of list_objs to a json file
+
     Attributes:
         __nb_objects: count of instances of Base
 
@@ -37,3 +41,15 @@ class Base:
         if len(list_dictionaries) < 1:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """write a json string representation of list_objs to a json file"""
+        json_file = cls.__name__ + ".json"
+        my_list = []
+
+        if list_objs is not None:
+            for obj in list_objs:
+                my_list.append(obj.to_dictionary())
+        with open(json_file, "w") as file:
+            json.dump(my_list, file)
