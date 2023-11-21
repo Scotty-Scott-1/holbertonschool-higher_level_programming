467673c134779c80ae096c7dc218cc124acd8a2d
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-21 16:07:21 +0100

    added task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- create a user: user_0d_1 for server. Set all privilages for server. Set password to user_0d_1_pwd
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
