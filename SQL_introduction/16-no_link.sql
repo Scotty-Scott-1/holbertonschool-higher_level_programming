3881a4ef2d7080a22d4fc825d7e918ac115d68ca
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-21 11:15:40 +0100

    updated 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
--- Display all rows of second_table. Don't select the column id. By score descensing
+-- Display all rows of second_table. Don't select the column id. By score descensing. Check for null values and empty strings
 SELECT score, name
 FROM second_table
+WHERE name != "" AND name IS NOT NULL
 ORDER BY score DESC;
