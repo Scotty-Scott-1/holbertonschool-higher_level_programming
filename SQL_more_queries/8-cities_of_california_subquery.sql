00717c37574c04af0f41b3841f932e1e7801dfcf
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-22 09:41:31 +0100

    corrected styleguide

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,6 @@
 -- Use a subquery to get the correct state id from states
 SELECT id, name FROM cities
 WHERE state_id = (
-	SELECT id from states
+	SELECT id FROM states
 	WHERE name = 'California'
 ) ORDER BY id ASC;
