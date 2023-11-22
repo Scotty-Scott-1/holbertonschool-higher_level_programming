fa3a9bcded03b906168cb56234644cf0b62d8ba1
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-22 16:00:05 +0100

    added task 11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Show all shows and their corresponding genre, else if no genre NULL
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
