d49d16bca47bca3771d3b62437243587d466c04d
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-26 00:08:10 +0100

    added task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A function that access the database lists all states that begin
+with command line argv 4.
+"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    state_name = argv[4]
+
+    query = ("SELECT * FROM states "
+             "WHERE name LIKE BINARY %s "
+             "ORDER BY states.id ASC")
+
+    cur = conn.cursor()
+    cur.execute(query, ("{}".format(state_name),))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
