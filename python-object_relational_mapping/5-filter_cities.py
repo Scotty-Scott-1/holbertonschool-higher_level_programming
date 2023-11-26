7650dcaed7a140f56a1c13278cb780f5a2983597
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-26 23:44:22 +0100

    Added task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+List all cities based on a state (argv 4). Safe from sql injection
+"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    state_name = argv[4]
+
+    query = ("SELECT cities.name "
+             "FROM cities "
+             "JOIN states ON states.id = cities.state_id "
+             "WHERE states.name LIKE BINARY %s "
+             "ORDER BY cities.id ASC;")
+
+    cur = conn.cursor()
+    cur.execute(query, ("{}".format(state_name),))
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row[0])
+
+    cur.close()
+    conn.close()
