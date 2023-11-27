a41bc2c5486559b732d8be4bead30e5d45bd35db
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-27 13:14:08 +0100

    added task 11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Add a new state to the table
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+import sys
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    Mysession = Session()
+
+    new = State(name="New York")
+    Mysession.add(new)
+    Mysession.commit()
+
+    print(new.id)
+
+    Mysession.close()
