51b1dcfa809e716416bc6eddbf6dabab2e0bd9f2
Scotty_Scott-1 <swilliamfr@outlook.com>
2024-01-31 12:57:01 +0100

    added task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,4 @@
+document.getElementById('update_header').addEventListener('click', function () {
+  const headerText = document.querySelector('header');
+  headerText.textContent = 'New Header!!!';
+});
